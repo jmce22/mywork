1f27720414ee26932de9c32dfabddad01cbbc59b
James McEneaney <jamesmceneaney17@gmail.com>
2023-02-07 23:40:24 +0000

    this code asks whether the number represented by a is equal to that represented by b

---
+++ week02/equalnumbers.py
@@ -0,0 +1,11 @@
+# This code asks whether the number on the left
+# equals the number on the right
+# reference is https://pythonexamples.org/python-program-check-if-two-numbers-are-equal/
+
+a = 2
+b = 3
+
+if ( a == b ) :
+    print('The two numbers are equal.')
+else :
+    print('The two numbers are not equal.')
