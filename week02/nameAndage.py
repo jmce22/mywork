0323ada95a739fe99415c2cd6f36e3b4b7f25aa7
James McEneaney <jamesmceneaney17@gmail.com>
2023-02-07 19:45:05 +0000

    extra code from week02

---
+++ week02/nameAndage.py
@@ -0,0 +1,7 @@
+# this program takes the users name and age 
+# and outputs a sentence giving both
+# author: James McEneaney
+
+name = input('please enter your name: ')
+age = input('please enter your age: ')
+print(f'Hello {name},\t your age is {age}')
---
+++ week02/subtractionsum.py
@@ -0,0 +1,6 @@
+# this code subtracts the two numbers found within it
+# and prints out the result
+# Author: James McEneaney
+
+ans = int(21 - 4)
+print(ans)
